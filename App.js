8ef0064fd23e51da894ee171bacdf7d76f32e335
Guxim <guxim.selmani1@hotmail.com>
2021-07-03 01:55:20 +0200

    Course Goals

--- App.js
+++ App.js
@@ -1,59 +1,41 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const GoalHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
   };
 
-  const AllGoals = () => {
-    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
-  };
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder=" Course goals"
-          style={styles.input}
-          onChangeText={GoalHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={AllGoals} />
-      </View>
-      <View>
-        {courseGoals.map((goal) => (
-          <View style={styles.items} key={goal}>
-            <Text>{goal}</Text>
-          </View>
-        ))}
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: { padding: 50 },
-  inputContainer: {
-    paddingTop: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  input: {
-    //borderColor: "black",
-    borderWidth: 1,
-    height: 35,
-    marginBottom: 2,
-    width: "80%",
-    textAlign: "center",
-  },
-  items: {
-    padding: 10,
-    marginVertical: 3,
-    backgroundColor: "#ccc",
-    borderWidth: 1,
+  screen: {
+    padding: 50,
   },
 });
