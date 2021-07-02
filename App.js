0fc15b25ac95635f0eea1347b2677149f6c667b0
Guxim <guxim.selmani1@hotmail.com>
2021-07-02 16:45:45 +0200

    Course goals

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const GoalHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const AllGoals = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder=" Course goals" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder=" Course goals"
+          style={styles.input}
+          onChangeText={GoalHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={AllGoals} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => (
+          <View style={styles.items} key={goal}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -29,5 +49,11 @@ const styles = StyleSheet.create({
     marginBottom: 2,
     width: "80%",
     textAlign: "center",
+  },
+  items: {
+    padding: 10,
+    marginVertical: 3,
+    backgroundColor: "#ccc",
+    borderWidth: 1,
   },
 });
